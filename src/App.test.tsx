fffda7c350f6906164516f0a4637a4ebd67f6e78
Schyler Evans <schyler.evans@gmail.com>
2024-02-02 21:57:44 -0800

    Undergoing renovations

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders LinkedIn link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("linkedin.com/in/schyler-evans");
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+import chaos from './chaos.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={chaos} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Undergoing renovations.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/schyler-evans/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          linkedin.com/in/schyler-evans
         </a>
       </header>
     </div>
