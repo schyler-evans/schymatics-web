bc6a57ed678e4984f630711f8b817207c14a58c1
Schyler Evans <schyler.evans@gmail.com>
2024-02-03 12:17:16 -0800

    rm all but the build

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders LinkedIn link', () => {
-  render(<App />);
-  const linkElement = screen.getByText("linkedin.com/in/schyler-evans");
-  expect(linkElement).toBeInTheDocument();
-});
